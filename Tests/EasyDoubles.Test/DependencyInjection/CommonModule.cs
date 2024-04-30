0725456bebba762fcb062615150bc2cd30a727c2
Tigran TIKSN Torosyan <personal@tiksn.com>
2024-04-30 20:38:08 +0200

    Update CommonModule.cs

--- Tests/EasyDoubles.Test/DependencyInjection/CommonModule.cs
+++ Tests/EasyDoubles.Test/DependencyInjection/CommonModule.cs
@@ -1,5 +1,17 @@
 namespace EasyDoubles.Test.DependencyInjection;
 
 using Autofac;
+using EasyDoubles.Test.Services;
 
-public class CommonModule : Module;
+public class CommonModule : Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        base.Load(builder);
+
+        _ = builder
+            .RegisterType<CatalogInitializer>()
+            .As<ICatalogInitializer>()
+            .InstancePerLifetimeScope();
+    }
+}
