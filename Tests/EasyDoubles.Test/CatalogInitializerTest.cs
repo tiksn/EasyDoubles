eb23ee35660afa74dd7ab83cecfafd336824aa64
Tigran TIKSN Torosyan <personal@tiksn.com>
2024-05-01 20:14:54 +0200

    Update CatalogInitializerTest.cs

--- Tests/EasyDoubles.Test/CatalogInitializerTest.cs
+++ Tests/EasyDoubles.Test/CatalogInitializerTest.cs
@@ -3,14 +3,20 @@ namespace EasyDoubles.Test;
 using EasyDoubles.Test.Services;
 using Microsoft.Extensions.DependencyInjection;
 using Xunit;
+using Xunit.Abstractions;
 
 public class CatalogInitializerTest
 {
+    private readonly ITestOutputHelper testOutputHelper;
+
+    public CatalogInitializerTest(ITestOutputHelper testOutputHelper)
+        => this.testOutputHelper = testOutputHelper ?? throw new ArgumentNullException(nameof(testOutputHelper));
+
     [Fact]
-    public async Task GivenTester_WhenInitialized_ThenAssertAll()
+    public async Task GivenTester_WhenInitialized_ThenAllMatch()
     {
         // Arrange
-        var tester = new MultiDatabaseTester();
+        var tester = new MultiDatabaseTester(this.testOutputHelper);
         await tester.InitializeAsync(default);
 
         // Act
